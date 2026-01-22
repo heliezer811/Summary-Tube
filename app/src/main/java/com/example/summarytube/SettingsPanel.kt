9447352503ddddfe0e52bb1c3ebe1f29771dc031
Heliézer <59342457+heliezer811@users.noreply.github.com>
2026-01-22 20:19:56 -0300

    Update SettingsPanel.kt

--- app/src/main/java/com/example/summarytube/SettingsPanel.kt
+++ app/src/main/java/com/example/summarytube/SettingsPanel.kt
@@ -3,10 +3,11 @@ package com.example.summarytube
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
