eae354794f6d32fb9e7324c8e1fb004d44f09cb2
Heliézer <59342457+heliezer811@users.noreply.github.com>
2026-01-29 22:39:58 -0300

    Update MainActivity.kt

--- app/src/main/java/com/summarytube/MainActivity.kt
+++ app/src/main/java/com/summarytube/MainActivity.kt
@@ -8,6 +8,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
