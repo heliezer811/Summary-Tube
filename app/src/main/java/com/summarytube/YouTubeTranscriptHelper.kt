c4bea4b8a14810b8d56eaa3bc391c18fde8f77af
Heliézer <59342457+heliezer811@users.noreply.github.com>
2026-02-01 22:19:39 -0300

    Update YouTubeTranscriptHelper.kt

--- app/src/main/java/com/summarytube/YouTubeTranscriptHelper.kt
+++ app/src/main/java/com/summarytube/YouTubeTranscriptHelper.kt
@@ -1,6 +1,6 @@
 package com.summarytube
 
-import android.util.Log
+//import android.util.Log
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import okhttp3.OkHttpClient
